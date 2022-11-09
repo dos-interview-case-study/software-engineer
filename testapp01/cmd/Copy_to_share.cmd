00b8fd97fc16450b1d4c845191696530834f94a1
dos-interview-case-study <117833755+dos-interview-case-study@users.noreply.github.com>
2022-11-09 10:23:34 -0500

    Update Copy_to_share.cmd

--- testapp01/cmd/Copy_to_share.cmd
+++ testapp01/cmd/Copy_to_share.cmd
@@ -1,7 +1,7 @@
 REM script to copy file to share
 set myErrors=0
 
-copy D:\temp\case_study\testapp01\output\test.txt \\rm15fs01\crms\
+copy \testapp01\output\test.txt \\rm15fs01\crms\
 if %errorlevel% NEQ 0 set /A myErrors+=1
 
 :end
