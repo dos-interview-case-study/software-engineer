94c7ab0efcbe498203b08f2fc0d5a337481993de
dos-interview-case-study <117833755+dos-interview-case-study@users.noreply.github.com>
2022-11-09 10:16:04 -0500

    Update script3.cmd

--- testdb01/cmd/script3.cmd
+++ testdb01/cmd/script3.cmd
@@ -2,14 +2,14 @@ REM script3.cmd - script to create csv
 set myErrors=0
 
 REM use input files to create output
-for /F "tokens=* delims=*" %%G in (D:\temp\case_study\testapp01\input\sql1.txt) do (
-@echo %%G,>>D:\temp\case_study\testapp01\output\analysis.csv
+for /F "tokens=* delims=*" %%G in (\testapp01\input\sql1.txt) do (
+@echo %%G,>>\testapp01\output\analysis.csv
 )
-for /F "tokens=* delims=*" %%G in (D:\temp\case_study\testapp01\input\sql2.txt) do (
-@echo %%G,>>D:\temp\case_study\testapp01\output\analysis.csv
+for /F "tokens=* delims=*" %%G in (\testapp01\input\sql2.txt) do (
+@echo %%G,>>\testapp01\output\analysis.csv
 )
 
-if not exist D:\temp\case_study\testapp01\output\analysis.csv set /A myErrors+=1
+if not exist \testapp01\output\analysis.csv set /A myErrors+=1
 
 :end
 exit /b %myErrors%
