c0ccad268b02331ad5f77f1a6340601daa06824e
dos-interview-case-study <117833755+dos-interview-case-study@users.noreply.github.com>
2022-11-09 10:09:00 -0500

    Update query2.cmd

--- testdb01/cmd/query2.cmd
+++ testdb01/cmd/query2.cmd
@@ -3,15 +3,15 @@ set myErrors=0
 
 REM get password
 d:
-call d:\temp\case_study\testdb01\cmd\db_login.cmd openffs_up
+call \testdb01\cmd\db_login.cmd openffs_up
 if %errorlevel% NEQ 0 goto badend
 
 REM run sql
-sqlplus -l %connectstring% @D:\temp\case_study\testdb01\sql\query2.sql
+sqlplus -l %connectstring% @\testdb01\sql\query2.sql
 if %errorlevel% NEQ 0 (set /A myErrors+=1)
 
 REM copy output
-copy d:\temp\case_study\testdb01\log\sql2.log D:\temp\case_study\testapp01\input\ 
+copy \testdb01\log\sql2.log \testapp01\input\ 
 if %errorlevel% NEQ 0 (set /A myErrors+=1)
 
 :end
